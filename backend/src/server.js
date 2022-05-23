c4f22b89431f35edd6769280a19b9ea89e067ac7
Weslen Mendes <weslenmends@gmail.com>
2022-05-23 00:24:09 -0300

    feat: implemented a controlled upload with multer

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,5 +1,8 @@
 import express, { json, urlencoded } from "express";
 import cors from "cors";
+import multer from "multer";
+
+import { storage, fileFilter } from "./config/multer.js";
 
 const app = express();
 const port = process.env.PORT || 5000;
@@ -8,8 +11,19 @@ app.use(cors());
 app.use(json());
 app.use(urlencoded({ extended: true }));
 
+const multerConfig = { storage: storage, fileFilter: fileFilter };
+const upload = multer(multerConfig).single("image");
+
 app.get("/", (req, res) => {
   res.send({ msg: "Aplicação está funcionando. ;)" });
+});
+
+app.post("/upload", upload, async (req, res) => {
+  if (!req.file) {
+    return res.status(422).send({ msg: "Envie um formato de arquivo válido." });
+  }
+
+  res.status(201).send({ msg: "Upload feito com sucesso!" });
 });
 
 app.listen(port, () => {
